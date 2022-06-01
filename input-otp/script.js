2f5d62b919ccbfef80868d7c3ed054af71657eda
tampt <tampt95@gmail.com>
2022-06-01 22:41:53 +0700

    udpate input opt

--- input-otp/script.js
+++ input-otp/script.js
@@ -9,14 +9,14 @@ const NUMBER_ONLY_REGEX = /^[0-9]$/;
 const inputOpts = document.querySelectorAll(".input-opt__input");
 
 inputOpts.forEach(addInputEvent);
-
+inputOpts[0].focus();
 function addInputEvent(element, index, inputList) {
   element.addEventListener("keydown", function (event) {
     if (event.keyCode === KEY_CODE_TAB) {
       return;
     }
 
-    // jump into next input when hitting arrow right
+    // jump into next input when hitt arrow right
     if (
       event.keyCode === KEY_CODE_ARROW_RIGHT &&
       index + 1 < inputList.length
@@ -24,19 +24,25 @@ function addInputEvent(element, index, inputList) {
       inputList[index + 1].focus();
       return;
     }
-    // jump into preview input when hitting arrow right
+    // jump into preview input when hitt arrow right
     if (event.keyCode === KEY_CODE_ARROW_LEFT && index - 1 >= 0) {
       inputList[index - 1].focus();
       return;
     }
+
+    // delete previous element when hit backspace
     if (event.keyCode === KEY_CODE_BACKSAPCE && index - 1 >= 0) {
-      inputList[index - 1].value = "";
-      inputList[index - 1].focus();
+      deleteChar(inputList, index - 1);
       event.preventDefault();
       return;
     }
 
-    // if (event.keyCode === KEY_CODE_DELETE)
+    if (event.keyCode === KEY_CODE_DELETE && index >= 0) {
+      deleteChar(inputList, index);
+      event.preventDefault();
+      return;
+    }
+
     if (!NUMBER_ONLY_REGEX.test(event.key)) {
       event.preventDefault();
       return;
@@ -45,21 +51,17 @@ function addInputEvent(element, index, inputList) {
     if (index + 1 < inputList.length) {
       inputList[index + 1].focus();
       event.preventDefault();
-    } else {
-      event.target.setSelectionRange(0, 0);
     }
+    event.target.setSelectionRange(0, 0);
   });
 
   element.addEventListener("focus", function (event) {
+    event.preventDefault();
     event.target.setSelectionRange(0, 0);
   });
   element.addEventListener("click", function (event) {
     event.preventDefault();
     event.target.setSelectionRange(0, 0);
-  });
-
-  element.addEventListener("keyup", function (event) {
-    console.log(event);
   });
 }
 
@@ -68,4 +70,13 @@ function isCursorLast(event) {
 }
 function isCursorFirst(event) {
   return event.target.selectionStart === 0;
+}
+
+function deleteChar(inputList, index) {
+  for (let i = index; i < inputList.length - 1; i++) {
+    inputList[i].value = inputList[i + 1].value;
+  }
+  inputList[inputList.length - 1].value = "";
+  inputList[index].focus();
+  inputList[index].setSelectionRange(0, 0);
 }
